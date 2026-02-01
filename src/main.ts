affd6a1289ba07b0c1dfce7c78b11eb27a28640d
Marko Rankovic <markosapps@gmail.com>
2026-02-01 09:09:36 +0000

    fix(dev): allow frontend cors and exclude frontend from tsconfig

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,9 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors({
+    origin: ['http://localhost:5173', 'http://127.0.0.1:5173'],
+  });
   app.useGlobalPipes(
     new ValidationPipe({
       whitelist: true,
